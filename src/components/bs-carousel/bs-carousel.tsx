260506925f9340c1c9e8afab4617888e7a1276d3
Rogala, Krzysztof <krogala@ea.com>
2017-11-01 15:05:46 -0700

    implemented carousel component properties

--- src/components/bs-carousel/bs-carousel.tsx
+++ src/components/bs-carousel/bs-carousel.tsx
@@ -11,16 +11,81 @@ export class Carousel {
   @Prop()
   interval: number;
 
+  @Prop()
+  keyboard: boolean;
+
+  @Prop()
+  pause: string | boolean;
+
+  @Prop()
+  ride: string;
+
+  @Prop()
+  wrap: boolean;
+
+  carousel() {
+    $(this.element).carousel();
+  }
+
+  cycle() {
+    $(this.element).carousel('cycle');
+  }
+
+  doPause() {
+    $(this.element).carousel('pause');
+  }
+
+  prev() {
+    $(this.element).carousel('prev');
+  }
+
+  next() {
+    $(this.element).carousel('next');
+  }
+
+  dispose() {
+    $(this.element).carousel('dispose');
+  }
+
   componentWillLoad() {
+    console.log('Carousel.componentWillLoad', this.interval, this.keyboard, this.pause, this.ride, this.wrap);
+
     this.element.classList.add('carousel');
     this.element.classList.add('slide');
+
+    if (typeof this.interval !== 'undefined') {
+      if (typeof this.interval === 'number') {
+        // console.log('number');
+        this.setAttribute('interval', this.interval.toString());
+      }
+    }
+
+    if (typeof this.keyboard !== 'undefined') {
+      this.setAttribute('keyboard', this.keyboard.toString());
+    }
+
+    if (typeof this.pause !== 'undefined') {
+      this.setAttribute('pause', this.pause.toString());
+    }
+
+    if (typeof this.ride !== 'undefined') {
+      this.setAttribute('ride', this.ride.toString());
+    }
+
+    if (typeof this.wrap !== 'undefined') {
+      this.setAttribute('wrap', this.wrap.toString());
+    }
+
   }
 
   componentDidLoad() {
-    $('.carousel').carousel();
+    console.log('Carousel.componentDidLoad', this.interval, this.keyboard, this.pause, this.ride, this.wrap);
+    this.carousel();
   }
 
   render() {
+    console.log('Carousel.render', this.interval, this.keyboard, this.pause, this.ride, this.wrap);
+
     return ([
       <div class="carousel-inner">
         <slot name="items" />
@@ -35,4 +100,11 @@ export class Carousel {
           </a>
           ]);
   }
+
+  private setAttribute(name: string, value: string) {
+    const typ = document.createAttribute('data-' + name);
+    typ.value = value;
+    this.element.attributes.setNamedItem(typ);
+  }
+
 }
