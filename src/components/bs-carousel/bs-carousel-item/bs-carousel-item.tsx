954b103b7084492fde3ec506f1454d4a76083b4a
Rogala, Krzysztof <krogala@ea.com>
2017-11-02 12:14:09 -0700

    implemented captions

--- src/components/bs-carousel/bs-carousel-item/bs-carousel-item.tsx
+++ src/components/bs-carousel/bs-carousel-item/bs-carousel-item.tsx
@@ -17,20 +17,40 @@ export class CarouselItem {
   @Prop()
   active = false;
 
+  hasContent = false;
+
   componentWillLoad() {
+    console.log('CarouselItem.componentWillLoad', this.element.children);
     this.element.classList.add('carousel-item');
+
+    this.hasContent = this.element.children.length > 0;
   }
 
   componentDidLoad() {
+    console.log('CarouselItem.componentDidLoad', this.element.children);
     if (this.active) {
       this.element.classList.add('active');
     }
   }
 
+  renderCaption() {
+    if (!this.hasContent) {
+      return;
+    } else {
+      return (
+        <div class="carousel-caption d-none d-md-block">
+          <slot />
+        </div>
+      )
+    }
+  }
+
   render() {
     console.log('CarouselItem->render');
-    return (
-      <img class="d-block w-100 img-fluid" src={this.src} alt={this.alt} />
+    return ([
+      <img class="d-block w-100 img-fluid" src={this.src} alt={this.alt} />,
+      this.renderCaption()
+    ]
     );
   }
 }
